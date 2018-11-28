16250490c74899c6747803c4a2295afeca8aa057
satpalkar1 <satpalkar.1@osu.edu>
2018-11-28 18:17:24 -0500

    Refactored FizzBuzz test case set up

--- tst/FizzBuzzTest.java
+++ tst/FizzBuzzTest.java
@@ -1,24 +1,27 @@
 import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzTest {
 
+	FizzBuzz fizzBuzz;
+
+	@Before
+	public void setUp(){
+		fizzBuzz= new FizzBuzz();
+	}
 	@Test
 	public void testWhenPassedNumberReturnNumber() {
-		FizzBuzz fizzBuzz= new FizzBuzz();
 		assertEquals("1",fizzBuzz.fizzBuzz(1));
 	}
 
 	@Test
 	public void testWhenPassedTwoReturnTwo() {
-		FizzBuzz fizzBuzz= new FizzBuzz();
 		assertEquals("2", fizzBuzz.fizzBuzz(2));
 	}
 
 	@Test
 	public void testWhenPassedNumDivisibleByThreeReturnFizz(){
-		FizzBuzz fizzBuzz= new FizzBuzz();
 		assertEquals("fizz", fizzBuzz.fizzBuzz(3));
 		assertEquals("fizz", fizzBuzz.fizzBuzz(6));
 	}
