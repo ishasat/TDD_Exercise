8df0e92e82cc36b89501baadfc710010c6246fb9
satpalkar1 <satpalkar.1@osu.edu>
2018-11-28 18:47:38 -0500

    Implemented solution for 1-100

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -17,5 +17,11 @@ public class FizzBuzz {
 
 		return returnValue;
 	}
+
+	public static void main(String[] args) {
+		FizzBuzz fb = new FizzBuzz();
+		for (int i = 1; i <= 100; i++) {
+			System.out.println(fb.fizzBuzz(i));
+		}
 }
 
